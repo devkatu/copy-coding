9c686a75693a34a17d529a2f6aca437f9f663464
katu1001 <katu1001@mineo.jp>
2023-07-04 01:41:39 +0900

    ちょこっと修正

--- js/script.js
+++ js/script.js
@@ -11,28 +11,26 @@ $(function () {
     });
 
     // ★slick
-    $('#portfolio_slick').slick({
-        accessibility: true,
+    $('.portfolio_slick').slick({
         autoplay: true,
         autoplaySpeed: 3500,
         arrows: true,
+        prevArrow: "<button class='slide-arrow slide-prev'></button>",
+        nextArrow: "<button class='slide-arrow slide-next'></button>",
         dots: true,
+        dotsClass: "slide-dots",
         centerMode: true,
-        slidesToShow: 1,
         speed: 500,
-        fade: false,
-        focusOnSelect: true,
-        variableWidth: true
     });
 
     // ★ハンバーガーメニュー用
-    $("#menu-btn").click(function () {
+    $(".menu-btn").click(function () {
         $(this).toggleClass("open");
         $(".header_nav").toggleClass("open");
     });
 
     // ★topへ戻るボタン
-    $('#go_top').hide();
+    $('.go_top').hide();
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('#go_top').fadeIn();
@@ -40,7 +38,7 @@ $(function () {
             $('#go_top').fadeOut();
         }
     });
-    $('#go_top').click(function () {
+    $('.go_top').click(function () {
         $('body,html').animate({
             scrollTop: 0
         }, 500);
