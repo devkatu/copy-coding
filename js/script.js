3a5804e1c04fb2c869a163cc6f4e150b0b1bb65e
katu1001 <katu1001@mineo.jp>
2023-07-04 11:41:43 +0900

    script修正

--- js/script.js
+++ js/script.js
@@ -1,60 +1,69 @@
 $(function () {
 
-    //  ★スムーススクロール用
-    $('a[href^="#"]').click(function () {
-        var speed = 500;
-        var href = $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top;
-        $("html,body").animate({ scrollTop: position }, speed, "swing");
-        return false;
-    });
+  // ★ハンバーガーメニュー用
+  $(".menu-btn").click(function () {
+    $(this).toggleClass("open");
+    $(".header_nav").toggleClass("open");
+  });
 
-    // ★slick
-    $('.portfolio_slick').slick({
-        autoplay: true,
-        autoplaySpeed: 3500,
-        arrows: true,
-        prevArrow: "<button class='slide-arrow slide-prev'></button>",
-        nextArrow: "<button class='slide-arrow slide-next'></button>",
-        dots: true,
-        dotsClass: "slide-dots",
-        centerMode: true,
-        speed: 500,
-    });
+  $('a[href^="#"]').click(function () {
+    // ハンバーガーメニューを閉じる
+    $(".menu-btn").removeClass("open");
+    $(".header_nav").removeClass("open");
 
-    // ★ハンバーガーメニュー用
-    $(".menu-btn").click(function () {
-        $(this).toggleClass("open");
-        $(".header_nav").toggleClass("open");
-    });
+    // 各aタグに指定されたhref属性を取得する
+    var href = $(this).attr("href");
 
-    // ★topへ戻るボタン
-    $('.go_top').hide();
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('#go_top').fadeIn();
-        } else {
-            $('#go_top').fadeOut();
-        }
-    });
-    $('.go_top').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 500);
-    });
+    // href属性で指定された要素(各section要素)をセレクタで指定し、位置を取得する
+    // ※offset()メソッドはtop,leftをプロパティに持つオブジェクトを返す
+    var pos = $(href).offset().top;
+
+    // 取得した位置へ1秒でアニメーションスクロールする
+    $("body,html").animate({ scrollTop: pos }, 500);
+
+    // aタグのデフォルト動作(画面遷移)を取り消す
+    return false;
+  });
+
+  // ★slick
+  $('.portfolio_slick').slick({
+    autoplay: true, // 自動再生を有効にする
+    autoplaySpeed: 3500, // 次のスライドへ移るまでの時間
+    arrows: true, // 前後の矢印を有効にする
+    prevArrow: "<button class='slide-arrow slide-prev'></button>", // 前へボタン
+    nextArrow: "<button class='slide-arrow slide-next'></button>", // 次へボタン
+    centerMode: true, // スライドを中央に表示する
+    dots: true, // ドットナビゲーションを有効にする
+    dotsClass: "slide-dots", // ドットナビのクラス名を指定
+    speed: 500, // スライドの移動にかかる時間
+  });
+
+  // topへ戻るボタンにクリックイベントを指定
+  $(".go-top").click(function () {
+    // <html>,<body>要素を1秒間のアニメーションしながらTOP位置へスクロール
+    $("html,body").animate({ scrollTop: 0 }, 500);
+  });
+  // windowにスクロールイベントを指定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      // windowのスクロール位置を取得し、100pxを超えたらTOPへ戻るボタンを表示
+      $(".go-top").fadeIn();
+    } else {
+      // windowのスクロール位置が100pxを超えなければTOPへ戻るボタンを非表示
+      $(".go-top").fadeOut();
+    }
+  });
 
 });
 
 
 $(window).load(() => {
-    // ★スクロールしたらふわっと表示
-    ScrollReveal().reveal('.srv', {
-        interval: 300,
-        origin: 'top',
-        duration: 700,
-        easing: 'ease-out',
-        distance: '50px'
-        // afterReveal: after1
-    });
+  // ★スクロールしたらふわっと表示
+  ScrollReveal().reveal('.reveal-item', {
+    origin: 'top', // 上から表示
+    distance: '50px', // 開始距離は50px離れたところから
+    duration: 700, // アニメーションを700ミリ秒に指定
+    interval: 300, // アニメーションが連続する際の間隔を300ミリ秒指定
+    easing: 'ease-out', // アニメーション効果を指定
+  });
 });
